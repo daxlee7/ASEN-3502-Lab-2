44abf5732523b067f895638d2d6f619d92153dc2
maspav2 <mapa1392@colorado.edu>
2026-09-25 12:28:02 -0600

    Add files via upload

--- bisection.m
+++ bisection.m
@@ -3,10 +3,26 @@ function [x, info] = bisection(f, fprime, interval, atol, maxit)
 a = interval(1);
 b = interval(2);
 
+% Increment Counter
+    func_count = 1; % Total calls so far
+    fa = f(a);
+
+% Initialize History Arrays
+    info.history.funccount = [];
+    info.history.x = [];
+
     for i = 1:maxit
 
         % Midpoint
         x = (a+b) / 2
+
+        % Func count at midpoint
+        fx = f(x);
+        func_count = func_count +1;
+
+        % Save history
+        info.history.funccount(end+1, 1) = func_count;
+        info.history.x(end+1, 1) = x;
 
         % Check if root is accurate
         if (b-a)/2 < atol
@@ -18,9 +34,8 @@ b = interval(2);
             b = x;
         else
             a = x;
+            fa = fx; % Save fx so no reevaluate f(a)
         end
     end
-
-info = struct();
 
 end
